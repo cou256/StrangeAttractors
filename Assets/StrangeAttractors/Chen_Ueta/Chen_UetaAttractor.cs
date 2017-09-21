a333ac97b5d73711a717e315a2f9cfc8d66ee5fb
Crimson_Apple <@Crimson_Apple>
2017-09-21 11:46:18 +0900

    Chen方程式、Lorentz方程式、Langford方程式、Ressler方程式を４次のRunge-Kutta法で近似を求め、アトラクタを描画するようにした。

---
+++ Assets/StrangeAttractors/Chen_Ueta/Chen_UetaAttractor.cs
@@ -0,0 +1,85 @@
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class Chen_UetaAttractor : MonoBehaviour
+{
+    [SerializeField] float a = 35.0f;
+    [SerializeField] float b = 3.0f;
+    [SerializeField] float c = 28.0f;
+
+    [SerializeField] int numberOfDraw = 200000;
+    [SerializeField] int initialIterate = 0;
+    [SerializeField] float speed = 1.0f;
+    [SerializeField] float particleScale = 0.1f;
+    [SerializeField] Bounds bounds = new Bounds(Vector3.zero, Vector3.one * 200.0f);
+
+    [SerializeField] Mesh mesh;
+    [SerializeField] Shader shader;
+    [SerializeField] ComputeShader kernel;
+
+    ComputeBuffer transformBuff;
+    ComputeBuffer argsBuff;
+    Material mat;
+
+    void KernelUpdate(float dt)
+    {
+        kernel.SetFloat("_A", a);
+        kernel.SetFloat("_B", b);
+        kernel.SetFloat("_C", c);
+        kernel.SetFloat("_DeltaTime", dt);
+        kernel.Dispatch(kernel.FindKernel("Calculate"), numberOfDraw / 8 + 1, 1, 1);
+    }
+    void OnDisable()
+    {
+        transformBuff.Release();
+        argsBuff.Release();
+        transformBuff = null;
+        argsBuff = null;
+        Destroy(mat);
+    }
+    void Start()
+    {
+        var data = new TransformStruct[numberOfDraw];
+        for (var i = 0; i < numberOfDraw; i++)
+        {
+            data[i].translate = Vector3.one;
+            data[i].scale = Vector3.one * particleScale;
+            data[i].speed = Random.Range(0.1f, 5.0f);
+            data[i].sumTime = i;
+        }
+        transformBuff = CreateComputeBuffer(data);
+        var args = new uint[5] { 0, 0, 0, 0, 0 };
+        args[0] = mesh.GetIndexCount(0);
+        args[1] = (uint)numberOfDraw;
+        argsBuff = new ComputeBuffer(1, args.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
+        argsBuff.SetData(args);
+        kernel.SetBuffer(kernel.FindKernel("Calculate"), "_TransformBuff", transformBuff);
+
+        mat = new Material(shader);
+        mat.SetBuffer("_TransformBuff", transformBuff);
+
+        for (int i = 0; i < initialIterate; i++)
+        {
+            KernelUpdate(1.0f / 60.0f);
+        }
+    }
+    void Update()
+    {
+        KernelUpdate(Time.deltaTime * speed);
+        Graphics.DrawMeshInstancedIndirect(mesh, 0, mat, bounds, argsBuff);
+    }
+    struct TransformStruct
+    {
+        public Vector3 translate;
+        public Vector3 rotation;
+        public Vector3 scale;
+        public float sumTime;
+        public float speed;
+    }
+    ComputeBuffer CreateComputeBuffer<T>(T[] data, ComputeBufferType type = ComputeBufferType.Default)
+    {
+        var computeBuffer = new ComputeBuffer(data.Length, Marshal.SizeOf(typeof(T)), type);
+        computeBuffer.SetData(data);
+        return computeBuffer;
+    }
+}
---
+++ Assets/StrangeAttractors/Rotate.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+	void Update () {
+        var ea = transform.eulerAngles;
+        ea.y += 0.1f;
+        transform.rotation = Quaternion.Euler(ea);
+    }
+}
